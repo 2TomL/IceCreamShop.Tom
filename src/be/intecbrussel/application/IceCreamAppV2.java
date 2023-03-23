d044c78633bc4f4d5965d4edd53d226833d938fe
2TomL <lamerstom.123@gmail.com>
2023-03-23 17:44:11 +0100

    Nog eens voor AppV2

--- src/be/intecbrussel/application/IceCreamAppV2.java
+++ src/be/intecbrussel/application/IceCreamAppV2.java
@@ -8,51 +8,61 @@ import be.intecbrussel.sellers.*;
 import java.util.Scanner;
 
 public class IceCreamAppV2 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         PriceList pricelist = new PriceList(1, 2, 3);
-
         IceCreamCar iceCreamCar = new IceCreamCar(pricelist, new Stock(83,30,50,60));
 
         System.out.println("Welcome to the Ice Cream Car! What would you like to order?");
         System.out.println("1. Cone \n2. Ice Rocket \n3. Magnum");
 
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("What flavors would you like in your cone?");
-                System.out.println("1. Banana \n2. Chocolate \n3. Vanilla \n4. Lemon \n5. Straciatella \n6. Mokka \n7. Pistachiooo");
+        boolean again = true;
+        while(again){
+            System.out.println("Welcome to the Ice Cream Salon! What would you like to order?");
+            System.out.println("1. Cone \n2. Ice Rocket \n3. Magnum");
+            System.out.println("or type 4 to exit");
 
-                int flavor1 = scanner.nextInt();
-                System.out.println("2nd Flavor:");
-                int flavor2 = scanner.nextInt();
-                System.out.println("3rd Flavor:");
-                int flavor3 = scanner.nextInt();
-                Cone.Flavor[] flavors = {Cone.Flavor.values()[flavor1 - 1], Cone.Flavor.values()[flavor2 - 1],Cone.Flavor.values()[flavor3 - 1]};
-                Cone cone = iceCreamCar.orderCone(flavors);
-                System.out.println("You have ordered a Cone with " + flavors[0] + ", " + flavors [1] + " and " + flavors[2] + " flavors");
-                cone.eat();
-                break;
-            case 2:
-                IceRocket iceRocket = iceCreamCar.orderIceRocket();
-                System.out.println("You have ordered an Ice Rocket");
-                iceRocket.eat();
-                break;
-            case 3:
-                System.out.println("What type of Magnum would you like?");
-                System.out.println("1. Classic MILKCHOCOLATE \n2. Classic WHITECHOCOLATE \n3. Classic BLACKCHOCOLATE \n4. Alpeanuts \n5. Romantic");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("What flavors would you like in your cone?");
+                    System.out.println("1. Banana \n2. Chocolate \n3. Vanilla \n4. Lemon \n5. Straciatella \n6. Mokka \n7. Pistachiooo");
 
-                int magnumType = scanner.nextInt();
-                Magnum.MagnumType [] types = {Magnum.MagnumType.values()[magnumType - 1]};
-                System.out.println("You have ordered a Magnum " + types);
-                System.out.println("The price of the Magnum " + types + " is " + types );
-                break;
-            default:
-                System.out.println("Invalid choice!");
-                break;
+                    int flavor1 = scanner.nextInt();
+                    System.out.println("2nd Flavor:");
+                    int flavor2 = scanner.nextInt();
+                    System.out.println("3rd Flavor:");
+                    int flavor3 = scanner.nextInt();
+                    Cone.Flavor[] flavors = {Cone.Flavor.values()[flavor1 - 1], Cone.Flavor.values()[flavor2 - 1],Cone.Flavor.values()[flavor3 - 1]};
+                    Cone cone = iceCreamCar.orderCone(flavors);
+                    System.out.println("You have ordered a Cone with " + flavors[0] + ", " + flavors [1] + " and " + flavors[2] + " flavors");
+                    cone.eat();
+                    break;
+                case 2:
+                    IceRocket iceRocket = iceCreamCar.orderIceRocket();
+                    System.out.println("You have ordered an Ice Rocket");
+                    iceRocket.eat();
+                    break;
+                case 3:
+                    System.out.println("What type of Magnum would you like?");
+                    System.out.println("1. Classic MILKCHOCOLATE \n2. Classic WHITECHOCOLATE \n3. Classic BLACKCHOCOLATE \n4. Alpeanuts \n5. Romantic");
+
+                    int magnumType = scanner.nextInt();
+                    Magnum.MagnumType type = Magnum.MagnumType.values()[magnumType - 1];
+                    iceCreamCar.orderMagnum(type);
+                    System.out.println("You have ordered a Magnum " + type.toString());
+                    break;
+                case 4:
+                    again = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+                }
+            }
+
+            System.out.println("Total profit for the Ice Cream Salon is: " + iceCreamCar.getProfit());
         }
-
-        System.out.println("Total profit for the Ice Cream Salon is: " + iceCreamCar.getProfit());
-    }
 }
